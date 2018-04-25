2335a8ab868de3e5838bc7a1994f2f0bd5c89039
Hugo BERTRAND <huber@P-LYO-CINDRE.dhcp.lyon.intranet>
2018-04-25 11:26:35 +0200

    Add license comment and scaladoc

--- src/main/scala/CustomSparkListener.scala
+++ src/main/scala/CustomSparkListener.scala
@@ -1,8 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark
 
 import org.apache.spark.scheduler._
 // import org.apache.spark.executor.TaskMetrics
 
+/**
+ * Spark listener aiming at registering values from chosen events
+ */
 class CustomSparkListener extends SparkListener {
   override def onApplicationStart(applicationStart: SparkListenerApplicationStart): Unit = {
     println(s"Application ${applicationStart.appName} started by ${applicationStart.sparkUser}")
@@ -12,41 +32,36 @@ class CustomSparkListener extends SparkListener {
     StatusHolder.maxMemory = blockManagerAdded.maxMem
   }
   override def onJobStart(jobStart: SparkListenerJobStart) {
-    println(s"Job started with ${jobStart.stageInfos.size} stages: $jobStart.")
-    val stageInfos = jobStart.stageInfos
-    stageInfos.foreach(i => println(s"""diskBytesSpilled on ${i.name}:
-      details: ${i.stageId}""")) //taskMetrics.diskBytesSpilled)))
+    // TODO
   }
 
   override def onTaskStart(taskStart: SparkListenerTaskStart): Unit = {
     val info = taskStart.taskInfo
     if (info != null) {
-      println(s"""[TASK] Task Info: ${info.taskId}""")
+      println(s"""Task ${info.taskId} started""")
     }
   }
   
   override def onExecutorMetricsUpdate(metricsUpdate: SparkListenerExecutorMetricsUpdate) {
-	if(metricsUpdate.accumUpdates.nonEmpty){
-	  println(s"Information is available for ${metricsUpdate.execId}:")
-	  for (i <- metricsUpdate.accumUpdates) {
-//      metricsUpdate.
-		  val accInfo: Seq[AccumulableInfo] = i._4
-		  accInfo.foreach(u => println(s"Info: task:${i._1} stage:${i._2} id:${u.id} name:${u.name} update:${u.update.get}"))
-	  }
-	}
+    if(metricsUpdate.accumUpdates.nonEmpty){
+      println(s"Information is available for ${metricsUpdate.execId}:")
+      for (i <- metricsUpdate.accumUpdates) {
+        val accInfo: Seq[AccumulableInfo] = i._4
+        accInfo.foreach(u => println(s"Info: task:${i._1} stage:${i._2} id:${u.id} name:${u.name} update:${u.update.get}"))
+      }
+    }
   }
 
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
     println(s"Stage ${stageCompleted.stageInfo.stageId} completed with ${stageCompleted.stageInfo.numTasks} tasks.")
   }
-  
-  override def onUnpersistRDD(unpersistRDD:  SparkListenerUnpersistRDD): Unit = {
-	println(s"RDD ${unpersistRDD.rddId} has been uncached.")
-  }
 }
 
+/**
+ * An object containing all the values to be displayed or used in the
+ * custom UI page
+ */
 object StatusHolder {
   var maxMemory: Long = -1
-  var currentExecutorMemory : Long = -1
 }
 
--- src/main/scala/CustomUITab.scala
+++ src/main/scala/CustomUITab.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.ui
 
 import javax.servlet.http.HttpServletRequest
@@ -8,16 +25,25 @@ import org.apache.spark.ui.{SparkUI, SparkUITab, UIUtils, WebUI, WebUIPage}
 import org.apache.spark.StatusHolder
 import org.apache.spark.SparkContext
 
+/**
+ * A standard class for an additional custom UI tab
+ *
+ * @param parent the UI registering the custom tab
+ */
 class CustomUITab(parent: SparkUI) extends SparkUITab(parent, "custom"){
   attachPage(new CustomWebUIPage(this))
 }
 
+/**
+ * A standard class for an additional page in a tab
+ *
+ * @param parent the tab the page is attached to
+ */
 class CustomWebUIPage(parent: SparkUITab) extends WebUIPage("") {
   def render(request: HttpServletRequest): Seq[Node] = {
     val content =
       <div>
         {
-          <div id="active-executors" class="row-fluid"></div> ++
           <p>This is a custom tab</p>
           <p>max memory
             {StatusHolder.maxMemory}
@@ -28,6 +54,7 @@ class CustomWebUIPage(parent: SparkUITab) extends WebUIPage("") {
   }
 }
 
+/** An object registering the custom tab in the SparkUI, from SparkContext */
 object SparkUIExtender {
   def extend(sparkContext: SparkContext): Unit = {
     val ui = sparkContext.ui
