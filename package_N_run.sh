1c82102b88c41c26d2540104139337aa84e8c7a4
HugoPopo <hu.bertrand@protonmail.com>
2018-04-26 11:27:00 +0200

    Update package_n_run.sh to match correct jar file

--- package_N_run.sh
+++ package_N_run.sh
@@ -5,4 +5,4 @@ then
     echo "launching sbt package...\n"
     sbt package
 fi
-spark-shell --conf spark.logConf=true --conf spark.extraListeners=org.apache.spark.CustomSparkListener --driver-class-path target/scala-2.11/custom-spark-ui_2.11-1.0.jar
+spark-shell --conf spark.logConf=true --conf spark.extraListeners=org.apache.spark.CustomSparkListener --driver-class-path target/scala-2.11/custom-spark-metrics-ui_2.11-1.0.jar
